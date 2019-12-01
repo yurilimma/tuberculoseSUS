b62f3ec76763dec83e6882d3bc4c47fd3d6d4176
Kaio Siqueira de Souza <siqueirakaio1510@gmail.com>
2019-12-01 08:12:34 -0300

    game screen

--- src/pages/community-game/community-game.ts
+++ src/pages/community-game/community-game.ts
@@ -16,12 +16,30 @@ import { GameService } from '../../services/game-service';
 })
 export class CommunityGamePage {
   public questionList: Array<any>;
+  public correctQuestion: Array<boolean>;
+  public incorrectQuestion: Array<boolean>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public gameService: GameService) {
     this.questionList = gameService.getAll();
+    this.correctQuestion = [];
+    this.incorrectQuestion = [];
+    this.questionList.forEach(element => {
+      this.correctQuestion.push(false);
+      this.incorrectQuestion.push(false);
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GameficationPage');
+  }
+
+  onClickTrue(questionConfirmation: boolean, i: number){
+    this.correctQuestion[i] = questionConfirmation[i] == true;
+    this.incorrectQuestion[i] = !this.correctQuestion[i];
+  }
+
+  onClickFalse(questionConfirmation: boolean, i: number){
+    this.incorrectQuestion[i] = questionConfirmation[i] == false;
+    this.correctQuestion[i] = !this.incorrectQuestion[i];
   }
 }
